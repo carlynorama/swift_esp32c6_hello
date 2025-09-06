4814f8e3c598a73a66db63d3d3d55a72ae25a1ee
carlynorama <carlynorama@gmail.com>
2025-09-06 14:43:04 -0700

    switch to delay()

--- main/Main.swift
+++ main/Main.swift
@@ -1,12 +1,12 @@
 @_cdecl("app_main")
 func main() {
   print("Hello from Swift on ESP32-C6!")
-  
+
   guard var led = DigitalIndicator(15) else {
     fatalError("Difficulty setting up pin.")
   }
 
-  guard var button = DigitalInput(9) else {
+  guard let button = DigitalInput(9) else {
     fatalError("Difficulty setting up button.")
   }
 
