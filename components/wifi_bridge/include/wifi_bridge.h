270babde88c137f26b5a83c924a3b4990dacd9ff
carlynorama <carlynorama@gmail.com>
2025-09-08 07:07:49 -0700

    tidy up

--- components/wifi_bridge/include/wifi_bridge.h
+++ components/wifi_bridge/include/wifi_bridge.h
@@ -6,12 +6,6 @@
 #include "esp_log.h"
 #include "nvs_flash.h"
 
-
-
-
-void wifi_bridge_connect_and_listen(void);
-
-
 int wifi_bridge_return_twelve(void);
 
 esp_err_t wifi_bridge_initialize_nvs(void);
@@ -19,7 +13,5 @@ esp_err_t wifi_bridge_initialize_nvs(void);
 esp_err_t wifi_bridge_initialize_netif(void);
 
 esp_err_t wifi_bridge_wifi_init_default_config(void);
-
-//esp_err_t wifi_bridge_wifi_set_start_config(const char* ssid, const char* pass);
 
 esp_err_t wifi_bridge_wifi_set_config_and_connect(const char *wifi_ssid, const char *wifi_pass);
